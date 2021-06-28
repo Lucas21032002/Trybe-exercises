c943c2822b88c24ab617394fd8f320e755501975
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2021-06-28 15:33:07 -0300

    Resolucao do exercicio 2 do bloco de funcoes

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-4.4.js
@@ -5,10 +5,26 @@ let info = {
     recorrente: 'Sim'
 }
 
-//for(let key in info){
-// console.log(key);
-//}
 
+//Exercicio 3//
+for(let key in info){
+ console.log(key);
+}
+
+//Exercicio 4//
 for(let key in info){
     console.log(info[key]);
 }
+
+// PARTE 2 FUNCOES//
+
+function verificaPalindrome(palavra){
+    let reverse = palavra.split('').reverse().join('');
+    if(reverse === palavra){
+        console.log('A palavra ' + palavra + " é um palindromo.");
+    }else{
+        console.log('A palavra ' + palavra + ' não é um palindromo.');
+    }
+}
+
+verificaPalindrome('ovo');
